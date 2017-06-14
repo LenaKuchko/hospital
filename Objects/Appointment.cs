ac3864d45745f0f57c583a8dc00f97244711cf3f
Joshua Fairchild and Olena Kuchko <joshua+olena@>
2017-06-14 10:46:00 -0700

    add class file for Appointment + constructors, get/sets, fail then pass test for all basic methods

---
+++ Objects/Appointment.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Hospital.Objects
+{
+  public class Appointment
+  {
+    public int Id {get; set;}
+    public int PatientId {get; set;}
+    public int DoctorId {get; set;}
+    public DateTime Date {get; set;}
+
+
+    public Appointment()
+    {
+      Id = 0;
+      PatientId = 0;
+      DoctorId = 0;
+      Date = default(DateTime);
+     }
+
+    public Appointment(DateTime date, int doctorId, int patientId, int id = 0)
+    {
+      Id = id;
+      DoctorId = doctorId;
+      PatientId = patientId;
+      Date = date;
+    }
+
+    public override bool Equals(System.Object otherAppointment)
+    {
+      if (!(otherAppointment is Appointment))
+      {
+        return false;
+      }
+      else
+      {
+        Appointment newAppointment = (Appointment) otherAppointment;
+        return (this.Id == newAppointment.Id &&
+                this.PatientId == newAppointment.PatientId &&
+                this.DoctorId == newAppointment.DoctorId &&
+                this.Date == newAppointment.Date);
+      }
+    }
+
+    public static List<Appointment>GetAll()
+    {
+      DB.CreateConnection();
+      DB.OpenConnection();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM appointments;", DB.GetConnection());
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      List<Appointment> allAppointments = new List<Appointment>{};
+      while (rdr.Read())
+      {
+        int id = rdr.GetInt32(0);
+        DateTime date = rdr.GetDateTime(1);
+        int patientId = rdr.GetInt32(2);
+        int doctorId = rdr.GetInt32(3);
+
+        Appointment newAppointment = new Appointment(date, doctorId, patientId, id);
+        allAppointments.Add(newAppointment);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      DB.CloseConnection();
+
+      return allAppointments;
+    }
+
+    public void Save()
+    {
+      DB.CreateConnection();
+      DB.OpenConnection();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO appointments (date, patient_id, doctor_id) OUTPUT INSERTED.id VALUES (@Date, @PatientId, @DoctorId);", DB.GetConnection());
+
+      cmd.Parameters.Add(new SqlParameter("@Date", this.Date));
+      cmd.Parameters.Add(new SqlParameter("@PatientId", this.PatientId));
+      cmd.Parameters.Add(new SqlParameter("@DoctorId", this.DoctorId));
+      Console.WriteLine(this.Date);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+      while(rdr.Read())
+      {
+        this.Id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      DB.CloseConnection();
+    }
+
+    public static Appointment Find(int searchId)
+    {
+      DB.CreateConnection();
+      DB.OpenConnection();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM appointments WHERE id = @AppointmentId;", DB.GetConnection());
+
+      cmd.Parameters.Add(new SqlParameter("@AppointmentId", searchId));
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      Appointment foundAppointment = new Appointment();
+      while (rdr.Read())
+      {
+        foundAppointment.Id = rdr.GetInt32(0);
+        foundAppointment.Date = rdr.GetDateTime(1);
+        foundAppointment.PatientId = rdr.GetInt32(2);
+        foundAppointment.DoctorId = rdr.GetInt32(3);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      DB.CloseConnection();
+      return foundAppointment;
+    }
+
+    public static void DeleteAll()
+    {
+      DB.CreateConnection();
+      DB.OpenConnection();
+
+      SqlCommand cmd = new SqlCommand("DELETE FROM appointments;", DB.GetConnection());
+      cmd.ExecuteNonQuery();
+      DB.CloseConnection();
+    }
+  }
+}
---
+++ Tests/AppointmentTests.cs
@@ -0,0 +1,76 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Hospital.Objects;
+
+namespace Hospital
+{
+  [Collection("Hospital")]
+
+  public class AppointmentTest : IDisposable
+  {
+    public AppointmentTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hospital_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void Appointment_GetAll_DatabaseEmptyOnload()
+    {
+      List<Appointment> testList = Appointment.GetAll();
+      List<Appointment> controlList = new List<Appointment>{};
+
+      Assert.Equal(controlList, testList);
+    }
+
+    [Fact]
+    public void Appointment_Save_SaveToDatabase()
+    {
+      Doctor newDoctor = new Doctor("Tom", "tom567", "567", "Cardiology");
+      newDoctor.Save();
+      Patient newPatient = new Patient("John", "john123", "123", new DateTime(1996, 04, 25));
+      newPatient.Save();
+      Appointment newAppointment = new Appointment(new DateTime(2017, 05, 21), newDoctor.Id, newPatient.Id);
+      newAppointment.Save();
+
+      Appointment testAppointment = Appointment.GetAll()[0];
+      Assert.Equal(newAppointment, testAppointment);
+    }
+
+    [Fact]
+    public void Appointment_Equals_AppointmentEqualsAppointment()
+    {
+      Doctor newDoctor = new Doctor("Tom", "tom567", "567", "Cardiology");
+      newDoctor.Save();
+      Patient newPatient = new Patient("John", "john123", "123", new DateTime(1996, 04, 25));
+      newPatient.Save();
+      Appointment controlAppointment = new Appointment(new DateTime(2017, 05, 21), newDoctor.Id, newPatient.Id);
+      Appointment testAppointment = new Appointment(new DateTime(2017, 05, 21), newDoctor.Id, newPatient.Id);
+
+      Assert.Equal(controlAppointment, testAppointment);
+    }
+
+    [Fact]
+    public void Appointment_Find_FindsAppointmentInDB()
+    {
+      Doctor newDoctor = new Doctor("Tom", "tom567", "567", "Cardiology");
+      newDoctor.Save();
+      Patient newPatient = new Patient("John", "john123", "123", new DateTime(1996, 04, 25));
+      newPatient.Save();
+      Appointment controlAppointment = new Appointment(new DateTime(2017, 05, 21), newDoctor.Id, newPatient.Id);
+      controlAppointment.Save();
+
+      Appointment testAppointment = Appointment.Find(controlAppointment.Id);
+
+      Assert.Equal(controlAppointment, testAppointment);
+    }
+
+    public void Dispose()
+    {
+      Appointment.DeleteAll();
+      Doctor.DeleteAll();
+      Patient.DeleteAll();
+    }
+  }
+}
